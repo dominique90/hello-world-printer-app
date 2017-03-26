e5bab3c9be52f1d465401990049820e24bd0ac68
root <you@example.com>
2017-03-26 12:34:35 +0200

    powinien nie dziala

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Dominik jest glupi"
+oje_imie = "Dominik jest glupi"
 msg = "Witaj swiecie!"
 
 @app.route('/')
